86ab414836106a9144acc4bedf9ed9fb931dd67a
Nouzbe <benjamin.amelot@gmail.com>
2015-06-24 21:37:16 +0200

    Instantiate bodyParser the right way

--- server.js
+++ server.js
@@ -4,19 +4,28 @@ var express 			= require('express'),
 	mongoose			= require('mongoose'),
 	meetupController 	= require('./server/controllers/meetup-controller');
 
+// connect to the db
 mongoose.connect('mongodb://localhost:27017/mean-demo');
 
-app.use(bodyParser());
+// instantiate bodyParser (that we use to parse http bodies)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+// the client app has access to the client directory
 app.use(express.static(__dirname+'/client'));
 
+// Express delivers index.html, from there on ngRoute is the boss
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/client/views/index.html');
 });
 
+// REST interface
 app.post('/api/meetups', meetupController.create);
 app.get('/api/meetups', meetupController.list);
 
+// let's start the server app for real
 app.listen(3000, function() {
 	console.log('I\'m listening.')
 })
