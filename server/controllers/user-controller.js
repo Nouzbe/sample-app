09f87a0b8cd52805173a0e294eac30169467b44a
Benjamin AMELOT <bamelot@NCEL21090.iis.amadeus.net>
2015-07-10 16:44:10 +0200

    Deleted dup files

--- server/controllers/user-controller.js
+++
@@ -1,8 +0,0 @@
-module.exports.login = function(req, res) {
-	var object = new UserObject(req.body);
-	console.log('Create : ' + object);
-	object.save(function (err, result){
-		res.json(result);
-	});
-}
-
